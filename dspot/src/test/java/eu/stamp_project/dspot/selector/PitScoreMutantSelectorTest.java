06aebd808cdf9f2115e3578f91abcabe5a74d0c3
andrewbwogi <bwogi.andrew@gmail.com>
2019-04-19 15:02:13 +0200

    duplication removal with pit

--- dspot/src/test/java/eu/stamp_project/dspot/selector/PitScoreMutantSelectorTest.java
+++ dspot/src/test/java/eu/stamp_project/dspot/selector/PitScoreMutantSelectorTest.java
@@ -2,10 +2,25 @@ package eu.stamp_project.dspot.selector;
 
 import eu.stamp_project.Utils;
 import eu.stamp_project.automaticbuilder.maven.DSpotPOMCreator;
+import eu.stamp_project.dspot.DSpot;
+import eu.stamp_project.dspot.amplifier.StringLiteralAmplifier;
 import eu.stamp_project.utils.AmplificationHelper;
 import eu.stamp_project.minimization.PitMutantMinimizer;
+import eu.stamp_project.utils.RandomHelper;
+import eu.stamp_project.utils.program.InputConfiguration;
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
+import org.junit.Test;
 import spoon.reflect.declaration.CtMethod;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Benjamin DANGLOT
@@ -19,6 +34,10 @@ public class PitScoreMutantSelectorTest extends AbstractSelectorTest {
                 - The amplified test should increase the mutation score of the test suite.
                     we compare the mutation score before and after.
          */
+
+    public static final String nl = System.getProperty("line.separator");
+
+    private static final char DECIMAL_SEPARATOR = (((DecimalFormat) DecimalFormat.getInstance()).getDecimalFormatSymbols().getDecimalSeparator());
 
     @Override
     @Before
@@ -60,4 +79,29 @@ public class PitScoreMutantSelectorTest extends AbstractSelectorTest {
     protected Class<?> getClassMinimizer() {
         return PitMutantMinimizer.class;
     }
+
+    @Test
+    public void testRemoveOverlappingTestsWithPitMutantScoreSelector() throws Exception {
+        try {
+            FileUtils.deleteDirectory(new File("target/trash"));
+        } catch (Exception ignored) {
+            //ignored
+        }
+        RandomHelper.setSeedRandom(23L);
+        InputConfiguration.initialize("src/test/resources/test-projects/test-projects.properties");
+        DSpot dspot = new DSpot(1, Arrays.asList(new StringLiteralAmplifier()), new PitMutantScoreSelector());
+        dspot.amplifyTestClass("example.TestSuiteDuplicationExample");
+        String path = InputConfiguration.get().getOutputDirectory() + System.getProperty("file.separator")
+                + "example.TestSuiteDuplicationExample" + "_mutants_report.txt";
+        try (BufferedReader buffer = new BufferedReader(new FileReader(path))) {
+            assertEquals(expectedReport, buffer.lines().collect(Collectors.joining(nl)));
+        }
+    }
+
+    private static final String expectedReport = AmplificationHelper.LINE_SEPARATOR +
+            "======= REPORT =======" + AmplificationHelper.LINE_SEPARATOR +
+            "PitMutantScoreSelector: " + AmplificationHelper.LINE_SEPARATOR +
+            "The original test suite kills 2 mutants" + AmplificationHelper.LINE_SEPARATOR +
+            "The amplification results with 1 new tests" + AmplificationHelper.LINE_SEPARATOR +
+            "it kills 3 more mutants";
 }
